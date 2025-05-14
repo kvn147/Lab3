50043f23f6cb3ce4ceeaa8fa572995599602dad5
PatrickR <96024158+PatrickRung@users.noreply.github.com>
2025-05-14 15:30:03 -0700

    removed UART 0 functionality
    added UART0 communication

--- Lab3Task2PartA/Lab3_Task2b.c
+++ Lab3Task2PartA/Lab3_Task2b.c
@@ -4,15 +4,18 @@
  * 
  */
 #include "Lab3_Task2b.h"
-
+#include "Lab3_Task2a.h"
+#include <stdio.h>
 void UART2_Init(void);
 
 int main(void) {
-    PLL_Init(PRESET2); // 60 MHz
+    UART_Init();
     UART2_Init(); // Initialize UART2
+    UARTDR_A = '9';
     while (1) {
-      while (!(UART2_FR_R & UART_FR_RXFE)) {
-        char c = (char)(UART2_DR_R & 0xFF); // Read the character from UART2
+      while (!(UARTFR_A & UART_FR_RXFE)) {
+        char c = (char)(UARTDR_A & 0xFF); // Read the character from UART2
+        printf("%c\n", c);
         while (UART2_FR_R & UART_FR_TXFF) {}
         UART2_DR_R = c; // Echo character to UART2
       }
@@ -27,15 +30,14 @@ void UART2_Init(void) {
     RCGCUART_R |= (1<<2); // Enable clock for UART2
     while (!(PRUART_R & (1<<2))) {} // Wait for clock to stabilize
 
-    GPIO_PORTA_AMSEL_R &= ~((1<<6)|(1<<7)); // Disable analog function on PA6 and PA7
     GPIO_PORTA_AFSEL_R |= (1<<6)|(1<<7); // Set PA6 and PA7 to alt function
     GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0x00FFFFFF) | (0x1 << 24) | (0x1 << 28); // Set PA6 and PA7 to UART mode
     GPIO_PORTA_DEN_R |= (1<<6)|(1<<7); // Enable digital function on PA6 and PA7
 
     UART2_CTL_R &= ~UART_CTL_UARTEN; // Disable UART2 to configure
-    UART2_IBRD_R = 390; // Set integer baud rate divisor 9600 baud rate
-    UART2_FBRD_R = 40; // Set fractional baud rate divisor
-    UART2_LCRH_R = UART_LCRH_WLEN_8; // Set line control register (8 data bits, no parity, 1 stop bit)
-    UART2_CC_R = 0; // Use system clock
+    UART2_IBRD_R = 104; // Set integer baud rate divisor 9600 baud rate
+    UART2_FBRD_R = 11; // Set fractional baud rate divisor
+    UART2_LCRH_R = 0x70; // Set line control register (8 data bits, no parity, 1 stop bit)
+    UART2_CC_R = 0x5; // Use system clock
     UART2_CTL_R |= (UART_CTL_UARTEN | UART_CTL_TXE | UART_CTL_RXE); // Enable UART2, TXE, RXE
 }
